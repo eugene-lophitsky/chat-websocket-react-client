3819acb4bd06e7cdf13b42105a67397cfd36707e
eugene-lophitsky <eugene-lophitsky@yandex.ru>
2025-02-14 12:38:10 +0300

    add routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,22 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router";
+import NotFoundPage from "./pages/NotFoundPage.jsx";
+import Home from "./pages/Home.jsx";
+import AuthPage from "./pages/AuthPage.jsx";
+import ChatPage from "./pages/ChatPage.jsx";
 import "./App.css";
-// import UsersList from "./components/UsersList/UsersList.jsx";
-// import { CheckButton } from "./components/CheckButton/CheckButton.jsx";
-import {useState} from "react";
-import ChatWindow from "./components/ChatWindow/ChatWindow.jsx";
-
 
 function App() {
-
-    const[currentUser, setCurrentUser] = useState({}| null);
-    const[isAuthenticated, setIsAuthenticated] = useState(false);
-
-  return (
-    <>
-      <div style={{ width: "300px" }}>
-        {/*<UsersList />*/}
-      </div>
-        {/*<CheckButton />*/}
-        <ChatWindow/>
-    </>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="*" element={<NotFoundPage />} />
+                <Route index element={<Home />} />
+                <Route path="auth-page" element={<AuthPage />} />
+                <Route path="chat-page" element={<ChatPage />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,12 @@
-import React, { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import {BrowserRouter, Routes, Route} from "react-router";
-
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 import "primereact/resources/themes/lara-light-cyan/theme.css";
-import 'primeicons/primeicons.css';
+import "primeicons/primeicons.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-      <BrowserRouter>
-          <App />
-      </BrowserRouter>
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+    <StrictMode>
+        <App />
+    </StrictMode>
+);
