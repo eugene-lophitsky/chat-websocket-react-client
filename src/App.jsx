d7bacb567b3a94166ab8cf1682d68d45014a082a
eugene-lophitsky <eugene-lophitsky@yandex.ru>
2025-02-14 21:17:23 +0300

    install redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router";
 import NotFoundPage from "./pages/NotFoundPage.jsx";
-import Home from "./pages/Home.jsx";
 import AuthPage from "./pages/AuthPage.jsx";
 import ChatPage from "./pages/ChatPage.jsx";
 import "./App.css";
@@ -10,10 +9,10 @@ function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="*" element={<NotFoundPage />} />
-                <Route index element={<Home />} />
+                <Route path="/" element={<Navigate to="auth-page"/>}/>
                 <Route path="auth-page" element={<AuthPage />} />
                 <Route path="chat-page" element={<ChatPage />} />
+                <Route path="*" element={<NotFoundPage />} />
             </Routes>
         </BrowserRouter>
     );
