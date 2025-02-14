95fbaf582aba6c8a54bba8322e081918d6ea63ab
eugene-lophitsky <eugene-lophitsky@yandex.ru>
2025-02-14 12:35:05 +0300

    add Pages

---
+++ src/pages/ChatPage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ChatWindow from "../components/ChatWindow/ChatWindow.jsx";
+
+function ChatPage () {
+    return (
+        <ChatWindow/>
+    )
+}
+
+export default ChatPage;
